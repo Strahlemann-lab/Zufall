0844670b09c0a54f7671d4781eebd77dde491170
Strahlemann <122629296+Strahlemann-lab@users.noreply.github.com>
2025-03-18 20:01:47 +0100

    Projektdateien hinzufügen.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace Zufall;
+
+public partial class MainWindow : Window
+{
+    private Random random = new Random();
+
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+
+    private async void Button_Click(object sender, RoutedEventArgs e)
+    {
+        if (ComboBox.SelectedItem != null)
+        {
+            if (int.TryParse(ComboBox.Text, out int maxZahl))
+            {
+                int zufallszahl = random.Next(1, maxZahl + 1);
+                for (int i = 0; i <= 10; i++)
+                {
+                    int zufallszahlNeu = random.Next(1, maxZahl + 1);
+                    ZahlLabel.Content = zufallszahlNeu.ToString();
+                    await Task.Delay(50);
+                }
+                ZahlLabel.Content = zufallszahl.ToString();
+                string zeitstempel = DateTime.Now.ToString("HH:mm:ss");
+                ListView.Items.Insert(0, new ListViewItem { Content = $"{zeitstempel} = {zufallszahl}", Focusable = false });
+            }
+        }
+    }
+}
