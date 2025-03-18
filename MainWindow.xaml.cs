3f04a2b723e134af10b25acc0d95a2570e35b0b2
Strahlemann <122629296+Strahlemann-lab@users.noreply.github.com>
2025-03-18 20:43:25 +0100

    More code security

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,11 +1,14 @@
 ﻿using System.Windows;
 using System.Windows.Controls;
+using System;
+
 
 namespace Zufall;
 
 public partial class MainWindow : Window
 {
     private Random random = new Random();
+    private bool activTask = false;
 
     public MainWindow()
     {
@@ -14,21 +17,41 @@ public partial class MainWindow : Window
 
     private async void Button_Click(object sender, RoutedEventArgs e)
     {
-        if (ComboBox.SelectedItem != null)
+        if (!activTask)
         {
-            if (int.TryParse(ComboBox.Text, out int maxZahl))
+            activTask = true;
+            Zufall_Activ(true);
+            if (ComboBox.SelectedItem != null)
             {
-                int zufallszahl = random.Next(1, maxZahl + 1);
-                for (int i = 0; i <= 10; i++)
+                if (int.TryParse(ComboBox.Text, out int maxZahl))
                 {
-                    int zufallszahlNeu = random.Next(1, maxZahl + 1);
-                    ZahlLabel.Content = zufallszahlNeu.ToString();
-                    await Task.Delay(50);
+                    int zufallszahl = random.Next(1, maxZahl + 1);
+                    for (int i = 0; i <= 10; i++)
+                    {
+                        int zufallszahlNeu = random.Next(1, maxZahl + 1);
+                        ZahlLabel.Content = zufallszahlNeu.ToString();
+                        await Task.Delay(50);
+                    }
+                    ZahlLabel.Content = zufallszahl.ToString();
+                    string zeitstempel = DateTime.Now.ToString("HH:mm:ss");
+                    ListView.Items.Insert(0, new ListViewItem { Content = $"{zeitstempel} = {zufallszahl}", Focusable = false });
                 }
-                ZahlLabel.Content = zufallszahl.ToString();
-                string zeitstempel = DateTime.Now.ToString("HH:mm:ss");
-                ListView.Items.Insert(0, new ListViewItem { Content = $"{zeitstempel} = {zufallszahl}", Focusable = false });
             }
+            activTask = false;
+            Zufall_Activ(false);
+        }
+    }
+    private void Zufall_Activ(bool x)
+    {
+        if (x)
+        {
+            Button_Zufall.IsEnabled = false;
+        }
+        else
+        {
+            Button_Zufall.IsEnabled = true;
         }
     }
 }
+
+
